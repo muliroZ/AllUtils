5c2eb1f7f91dec9d055bf97cdf3de6a843850d92
muliroZ <murilomega100@gmail.com>
2025-12-13 21:47:34 -0300

    projeto iniciado

---
+++ src/test/java/com/muriloscorp/allutils/AllUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muriloscorp.allutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
